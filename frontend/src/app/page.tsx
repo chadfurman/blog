43932249b1124aa0c16326bb3765f64fd6ea4737
Chad Furman <chad@chadfurman.com>
2024-11-15 21:44:25 -0500

    initial commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import {red} from "next/dist/lib/picocolors";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-screen"></div>
+  );
+}
